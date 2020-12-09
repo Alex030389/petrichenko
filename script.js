d692c86a00f74477ad9d35bd0bcbbee4dc45923d
Alex test user <x01z@mail.ru>
2020-12-09 19:37:05 +0300

    add personalMovieDB

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const lastMovie = prompt('Один из последних просмотренных фильмов?',''),
+      rateMovie= prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[lastMovie] = rateMovie;
+
+console.log(personalMovieDB);
